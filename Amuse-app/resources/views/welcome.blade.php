68b4f434294383ed21d2c0e4c2a6b830524d0f53
Nemoda Márk  Levente <mark.nemoda@gmail.com>
2023-03-06 22:11:08 +0100

    Amuse_web-1.0

---
+++ Amuse-app/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+@section('content')
+    <div class="row">
+        <div class="col">
+            <h1 id="title">Amuse</h1>
+
+                <div class="col-6">
+                    <p id="aboutUs"></p>
+                </div>
+                <div class="col-6">
+                    <p id="aboutUs"></p>
+                </div>
+        </div>
+    </div>
+@endsection
