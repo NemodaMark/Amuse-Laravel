6678e357c8bf40c1c7f1fa6135ac49fe0dcaa3b1
Nemoda Márk  Levente <mark.nemoda@gmail.com>
2023-03-28 13:59:26 +0200

    First steps to set up the webpage

--- Amuse-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ Amuse-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique()->default(" ");
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->double('balance')->default(0);
+            $table->integer('role')->default(1);
         });
     }
 
--- Amuse-app/resources/views/gameUpload.blade.php
+++ Amuse-app/resources/views/gameUpload.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('views.layout')
 @section('content')
 
 @endsection
